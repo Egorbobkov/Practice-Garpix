0a6039bd16a992ed09cf9f79456b26154ac48581
Egorbobkov <114438492+Egorbobkov@users.noreply.github.com>
2024-07-20 17:12:37 +0300

    Update all app

--- django_test/settings.py
+++ django_test/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'django_test.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'BACKEND': 'django.template.backends.'
+                   'django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
